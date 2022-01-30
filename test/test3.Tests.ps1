170606e7abc04db56489b4a11ab9b0f55138e994
unfkbvlbl <kevinmoore9@gmail.com>
2022-01-30 14:18:13 +0000

    Create test3.Tests.ps1

---
+++ test/test3.Tests.ps1
@@ -0,0 +1,12 @@
+Describe "Describe Test 3" {
+
+    Context "Look for Keys" {
+
+        It "Does Key: <_> Exist " -ForEach @( 
+            "NCDS-DB", "VG-DB", "FunDB"
+        ){
+            
+            $_ | Should -Be 'FunDB'
+        }
+    }
+}
